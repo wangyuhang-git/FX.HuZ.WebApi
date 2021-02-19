4150396a1609cdd24ed0851360c5b24dae9d6a15
宇航 王 <wangyuhang89@outlook.com>
2021-02-19 21:21:38 +0800

    增加根据字典查询的数据的功能，为app提供相关接口

---
+++ FX.HuZ.WebApi/Business/DictionaryBusiness.cs
@@ -0,0 +1,23 @@
+﻿using FX.FP.Busines.NewDAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace FX.HuZ.WebApi.Business
+{
+    public class DictionaryBusiness
+    {
+        protected Dictionary_Dal comdal = new Dictionary_Dal("SqlServer_FP_DB");
+        /// <summary>
+        /// 根据字典编码获取字典
+        /// </summary>
+        /// <param name="dictionaryCode">编码</param>
+        public DataTable GetDictionaryList(string dictionaryCode)
+        {
+            DataTable dt = comdal.GetDictionaryList(dictionaryCode);
+            return dt;
+        }
+    }
+}
---
+++ FX.HuZ.WebApi/Controllers/DictionaryController.cs
@@ -0,0 +1,36 @@
+﻿using FX.HuZ.WebApi.Business;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FX.HuZ.WebApi.Controllers
+{
+    public class DictionaryController : ApiController
+    {
+        DictionaryBusiness dictionaryBusiness = new DictionaryBusiness();
+
+        /// <summary>
+        /// 根据字典值获取字典数据列表
+        /// </summary>
+        /// <param name="dictionaryCode"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] dynamic value)
+        {
+            DataTable dt = new DataTable("DictionaryList");
+            if (!string.IsNullOrEmpty(value.dictionaryCode.ToString()))
+            {
+                dt = dictionaryBusiness.GetDictionaryList(value.dictionaryCode.ToString());
+            }
+            else
+            {
+                throw new ArgumentNullException("参数不能为空！");
+            }
+            return Json(dt);
+        }
+    }
+}
